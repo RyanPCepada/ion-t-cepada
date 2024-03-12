7cc2b7a4f55a0b12cf6e0c46124f75b3be973095
RyanPCepada <ryancepada1992@gmail.com>
2024-03-12 23:12:03 +0800

    made some enhancements

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonAlert, IonButton, IonActionSheet } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -7,18 +8,65 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Profile</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
+        
+    <IonCard>
+      <img alt="Silhouette of mountains" src="../src/assets/img/avatar.jpg" />
+        <IonCardHeader>
+          <IonCardTitle>Ryan P. Cepada</IonCardTitle>
+           <IonCardSubtitle>NBSC-BSIT Student</IonCardSubtitle>
+            </IonCardHeader>
+
+            <IonCardContent>&#x1F64C; Let God Move &#x1F64C;</IonCardContent>
+
+
+      <IonButton id="present-alert" expand="block" color="primary">Quick Facts</IonButton>
+      <IonAlert
+        trigger="present-alert"
+        header="Would you like to explore Quick Facts?"
+        subHeader="Unlock valuable insights"
+        message="Discover a wealth of information with Quick Facts."
+        buttons={['Learn More']}
+      ></IonAlert>
+
+<IonButton id="open-action-sheet" expand="block" color="primary">Open Action Sheet</IonButton>
+      <IonActionSheet
+        trigger="open-action-sheet"
+        header="Actions"
+        buttons={[
+          {
+            text: 'Delete',
+            role: 'destructive',
+            data: {
+              action: 'delete',
+            },
+          },
+          {
+            text: 'Share',
+            data: {
+              action: 'share',
+            },
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            data: {
+              action: 'cancel',
+            },
+          },
+        ]}
+      ></IonActionSheet>
+    </IonCard>
+        </IonContent>
+      </IonPage>
   );
 };
 
