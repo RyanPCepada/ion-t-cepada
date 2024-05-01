024e8d7679fae1c36613113c90f9ea62cfc28071
RyanPCepada <ryancepada1992@gmail.com>
2024-05-01 12:39:26 +0800

    fixing some issue

--- src/pages/Todolist/Todolist.tsx
+++ src/pages/Todolist/Todolist.tsx
@@ -167,7 +167,7 @@ const TodoList: React.FC = () => {
           <IonButtons slot="start">
             <IonBackButton defaultHref="/" />
           </IonButtons>
-          <IonTitle>Todos</IonTitle>
+          <IonTitle>Todo</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
