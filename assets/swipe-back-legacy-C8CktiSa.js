5dd59be8e9c7966973f13df7e28079e48e202b5d
RyanPCepada <ryancepada1992@gmail.com>
2024-05-03 08:20:36 +0800

    Updates

--- assets/swipe-back-legacy-C8CktiSa.js
+++ assets/swipe-back-legacy-C8CktiSa.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B9Vk2i8U.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BoIauC0O.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
