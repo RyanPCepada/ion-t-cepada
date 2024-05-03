6383228ac62702df9fe968bf93eac946c7ad8668
RyanPCepada <ryancepada1992@gmail.com>
2024-05-03 21:03:19 +0800

    Updates

--- assets/ios.transition-legacy-BuvoozIu.js
+++ assets/ios.transition-legacy-BuvoozIu.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B0338w5P.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-PZfzghOa.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
