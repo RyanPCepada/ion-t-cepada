ac918ac0ff8e7971926663ef2170aaa036a75d88
RyanPCepada <ryancepada1992@gmail.com>
2024-05-03 08:24:55 +0800

    Updates

--- assets/keyboard2-legacy-CbGGeuqi.js
+++ assets/keyboard2-legacy-CbGGeuqi.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BoIauC0O.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-CehmHTIP.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
