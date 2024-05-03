6a00884b75f40c73388aa6bc1ef4d6912a1ba6b5
RyanPCepada <ryancepada1992@gmail.com>
2024-05-03 20:47:40 +0800

    Updates

--- assets/keyboard2-legacy-CCa_Inm1.js
+++ assets/keyboard2-legacy-CCa_Inm1.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D1Tax1Oy.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-B0338w5P.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
