69368de010eb154d8fe42bb72bcfcabc0aa3207f
RyanPCepada <ryancepada1992@gmail.com>
2024-05-03 08:58:52 +0800

    Updates

--- assets/ios.transition-legacy-D7SND3y2.js
+++ assets/ios.transition-legacy-D7SND3y2.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CehmHTIP.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-D1Tax1Oy.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-D8_FyEnU.js
+++ assets/keyboard2-legacy-D8_FyEnU.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CehmHTIP.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-D1Tax1Oy.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
