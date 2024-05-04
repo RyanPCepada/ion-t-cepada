0d64ac9077169d31d9d9506e09552718d92a8920
RyanPCepada <ryancepada1992@gmail.com>
2024-05-04 10:52:43 +0800

    Updates

--- assets/md.transition-legacy-7lXvZssN.js
+++ assets/md.transition-legacy-7lXvZssN.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-PZfzghOa.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-CY1S4n2R.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
